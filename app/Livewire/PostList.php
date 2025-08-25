194c5e1b789c98859fa8fba9d2080d923bc17ab3
Wizzy Dev <wizzypump@gmail.com>
2025-08-25 09:38:07 +0800

    initial Commit

---
+++ app/Livewire/PostList.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Livewire;
+
+use App\Models\Post;
+use Livewire\Attributes\Computed;
+use Livewire\Attributes\On;
+use Livewire\Attributes\Url;
+use Livewire\Component;
+
+class PostList extends Component
+{
+
+    #[Url()]
+    public $sort = 'desc';
+
+    #[Url()]
+    public $search = '';
+
+    public function setSort($sort)
+    {
+        $this->sort = ($sort === 'desc') ? 'desc' : 'asc';
+    }
+    #[On('search')]
+    public function updateSearch($search)
+    {
+        $this->search = $search;
+    }
+
+    #[Computed()] // c'est grace a ceci qu'on peut utilser l'attribut $posts dans le rendering avec le $this
+    public function posts()
+    {
+        return Post::published()
+            ->orderBy('published_at', $this->sort)
+            ->where('title', 'like', '%' . $this->search . '%')
+            ->paginate(5);
+    }
+    public function render()
+    {
+        return view('livewire.post-list');
+    }
+}
